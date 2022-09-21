ce013cdb048501c5ae4eee87d632a5c3910f4cef
Toybear1986 <vaskorobogatov@yandex.ru>
2022-09-21 20:36:29 +0300

    TestNG with tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,35 +7,35 @@ import static org.testng.Assert.assertEquals;
 public class CashbackHackServiceTest {
 
     @Test
-    public void CheckRemainIfAmount900() {
+    public void checkRemainIfAmount900() {
         CashbackHackService service = new CashbackHackService();
         int expected = 100;
         assertEquals(service.remain(900), expected);
     }
 
     @Test
-    public void CheckRemainIfAmountZero() {
+    public void checkRemainIfAmountZero() {
         CashbackHackService service = new CashbackHackService();
         int expected = 1000;
         assertEquals(service.remain(0), expected);
     }
 
     @Test
-    public void CheckRemainIfAmountEqualsBoundary() {
+    public void checkRemainIfAmountEqualsBoundary() {
         CashbackHackService service = new CashbackHackService();
         int expected = 0;
         assertEquals(service.remain(1000), expected);
     }
 
     @Test
-    public void CheckRemainIfAmountMoreThanBoundary() {
+    public void checkRemainIfAmountMoreThanBoundary() {
         CashbackHackService service = new CashbackHackService();
         int expected = 0;
         assertEquals(service.remain(1001), expected);
     }
 
     @Test
-    public void CheckRemainIfAmountIsNegative() {
+    public void checkRemainIfAmountIsNegative() {
         CashbackHackService service = new CashbackHackService();
         int expected = 0;
         assertEquals(service.remain(-100), expected);
